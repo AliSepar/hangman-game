f92bdbe0a309059e5ea28356e61cf4705ae52723
AliSepar <alisabah.separ1@gmail.com>
2024-06-17 11:25:09 +0200

    adding files

---
+++ app.js
@@ -0,0 +1,7 @@
+import { wordList } from "./word-list";
+
+const worddysplay = document.querySelector(".word-dysplayed");
+const hintext = document.querySelector(".hint-text");
+const gussesstext = document.querySelector(".guesses-text");
+const keyboard = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector("img");
